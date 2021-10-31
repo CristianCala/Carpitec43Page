27530d5ea1762d6dd13d0cb2fcaec2bf0aab0670
CristianCala <cristiancala@protonmail.com>
2021-10-31 12:54:04 -0430

    Add animation of layer

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@ window.onload = function() {
  console.log("Script funcionando");
 
 }
+
+var uno = anime({
+  targets: '.layer',
+  translateY: 5,
+  direction: 'alternate',
+  loop: true,
+  easing: 'easeInOutQuad',
+  autoplay: true
+});
+
+function loop(t) {
+  animation.tick(t);
+  customRAF = requestAnimationFrame(loop);
+}
+
+requestAnimationFrame(loop);
+
+var slowAnimation = anime({
+  targets: '.layer-2',
+  translateY: 8,
+  direction: 'alternate',
+  loop: true,
+  easing: 'easeInOutQuad',
+  autoplay: true
+});
